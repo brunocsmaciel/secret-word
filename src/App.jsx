999fe12814b3ddc6d4611b2f6464c4689078f057
brunocsmaciel <branis.cesar18@gmail.com>
2025-01-04 10:55:55 -0300

    feat: add games components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
-import './App.css'
-import StartScreen from './components/StartScreen'
+import { useState } from "react";
+import "./App.css";
+import { wordsList } from "./data/words";
+
+import StartScreen from "./components/StartScreen";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 function App() {
+  const [gameStage, setGameState] = useState(stages[0].name);
+  const [words] = useState(wordsList)
+
+  console.log(words)
 
   return (
     <>
-    <div className='start'>
-     <StartScreen />
-    </div>
+      <div className="start">
+        {gameStage === 'start' && <StartScreen />}
+        {gameStage === 'game' && <Game />}
+        {gameStage === 'end' && <GameOver />}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
